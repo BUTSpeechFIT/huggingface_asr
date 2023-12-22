615f739387acab70e5096e49b8053fcced5b5f18
lakoc <xpolok03@stud.fit.vutbr.cz>
2023-12-22 02:04:06 +0100

    LoCo ASR - initial batch script

---
+++ recipes/tedlium/loco.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/bash
+#SBATCH --job-name TED
+#SBATCH --account OPEN-28-57
+#SBATCH --partition qgpu
+#SBATCH --gpus 4
+#SBATCH --nodes 1
+#SBATCH --time 2-00:00:00
+#SBATCH --output=/mnt/proj1/open-28-58/lakoc/huggingface_asr/outputs/ebranchformer_tedlium_loco1.out
+
+EXPERIMENT="ebranchformer_tedlium_loco1"
+PROJECT="LoCo"
+WORK_DIR="/mnt/proj1/open-28-58/lakoc/huggingface_asr"
+ENV_DIR="/mnt/proj1/open-28-58/lakoc/LoCo-ASR"
+RECIPE_DIR="${WORK_DIR}/recipes/tedlium"
+
+export HF_HOME="/scratch/project/open-28-57/lakoc/huggingface_cache"
+export PYTHONPATH="${PYTHONPATH}:${WORK_DIR}/src"
+export OMP_NUM_THREADS=64
+export WANDB_PROJECT=$PROJECT
+export WANDB_RUN_ID="${EXPERIMENT}"
+
+conda deactivate
+source activate loco_asr
+
+EXPERIMENT_PATH="${WORK_DIR}/experiments/${EXPERIMENT}"
+
+cd $WORK_DIR
+
+args=(
+  # General training arguments
+  --output_dir=$EXPERIMENT_PATH
+  --per_device_train_batch_size="64"
+  --per_device_eval_batch_size="64"
+  --dataloader_num_workers="24"
+  --num_train_epochs="150"
+  --bf16
+  --do_train
+  --do_evaluate
+  --joint_decoding_during_training
+  --load_best_model_at_end
+
+  # Optimizer related arguments
+  --optim="adamw_torch"
+  --learning_rate="2e-3"
+  --warmup_steps="20000"
+  --early_stopping_patience="5"
+  --weight_decay="1e-6"
+  --max_grad_norm="5.0"
+  --lsm_factor="0.1"
+  --gradient_accumulation_steps="1"
+
+  # Logging, saving and evaluation related arguments
+  --report_to="wandb"
+  --logging_steps="10"
+  --save_strategy="epoch"
+  --evaluation_strategy="epoch"
+  --wandb_predictions_to_save=50
+  --greater_is_better="False"
+  --save_total_limit="5"
+  --track_ctc_loss
+
+  # Data related arguments
+  --max_duration_in_seconds="20.0"
+  --min_duration_in_seconds="0.0"
+  --length_column_name="input_len"
+  --remove_unused_columns="False"
+  --preprocessing_num_workers="64"
+  --writer_batch_size="1000"
+  --dataset_name="LIUM/tedlium"
+  --dataset_config="release3"
+  --test_splits validation test
+
+  # Preprocessing related arguments
+  --data_preprocessing_config="${RECIPE_DIR}/data_preprocessing.json"
+
+  # Model related arguments
+  --from_encoder_decoder_config
+  --tokenizer_name="Lakoc/ted_uni500"
+  --feature_extractor_name="Lakoc/log_80mel_extractor_16k"
+  --base_encoder_model="Lakoc/fisher_ebranchformer_enc_12_layers_fixed"
+  --base_decoder_model="Lakoc/gpt2_tiny_decoder_6_layers"
+  --ctc_weight="0.3"
+  --decoder_pos_emb_fixed
+  --expect_2d_input
+
+  # Generation related arguments
+  --num_beams="4"
+  --max_length="512"
+  --predict_with_generate
+  --decoding_ctc_weight="0.3"
+  --eval_beam_factor="10"
+
+  # Context related arguments
+  --conv_ids_column_name="recording_id"
+  --turn_index_column_name="turn_id"
+  --enc_memory_cells_location 9 10 11
+  --dec_memory_cells_location 3 4 5
+  --enc_memory_dim 16
+  --dec_memory_dim 16
+)
+
+python src/trainers/train_enc_dec_asr_with_context.py "${args[@]}"
