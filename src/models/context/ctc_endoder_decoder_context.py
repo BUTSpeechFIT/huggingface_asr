masking fixes loco

src/models/context/ctc_endoder_decoder_context.py
@@ -27,8 +27,9 @@ from models.ctc_encoder_plus_autoregressive_decoder import (
 )
 
 
-class ContextManager:
-    def __init__(self, layer, config):
+class ContextManager(nn.Module):
+    def __init__(self, layer, config, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.layer = layer
         self.memory_dim = config.memory_dim
         self.hidden_states = {}
@@ -125,8 +126,8 @@ class ContextManager:
             for conv_id, hidden_state, memory_state, hidden_len in zip(
                 self.current_conversations, hidden_states, memory_states, hidden_lens
             ):
-                self.hidden_states[conv_id] = hidden_state[:hidden_len].detach().clone()
-                self.context_vectors[conv_id] = memory_state.detach().clone()
+                self.hidden_states[conv_id] = hidden_state[:hidden_len].clone().detach()
+                self.context_vectors[conv_id] = memory_state.clone().detach()
             if dist.is_available() and dist.is_initialized():
                 self.synchronize_states()
 
@@ -150,7 +151,8 @@ class ContextManager:
 
             if attention_mask is not None:
                 attention_mask_mha = (
-                    attention_mask.squeeze(dim=1)[:, :1, ...]
+                    attention_mask.clone()
+                    .squeeze(dim=1)[:, :1, ...]
                     .repeat_interleave(self.memory_dim, dim=1)
                     .repeat_interleave(self.layer.memory.output_attention.num_heads, dim=0)
                     .transpose(1, 2)
@@ -159,31 +161,38 @@ class ContextManager:
 
             else:
                 hidden_lens = self.input_id_lens
-                attention_mask_mha = torch.zeros(
+                attention_mask_mha = -torch.zeros(
                     (len(hidden_lens), hidden_states.size(1), self.memory_dim),
                     device=hidden_states.device,
-                    dtype=torch.float32,
+                    dtype=hidden_states.dtype,
                 )
                 for index, hidden_len in enumerate(hidden_lens):
-                    attention_mask_mha[index, hidden_len:, :] = torch.finfo(torch.float32).min
+                    attention_mask_mha[index, hidden_len:, :] = torch.finfo(hidden_states.dtype).min
                 attention_mask_mha = attention_mask_mha.repeat_interleave(
                     self.layer.memory.output_attention.num_heads, dim=0
                 )
+                # attention_mask_mha = None
 
             memory_mask = (
                 pad_sequence(
                     [
-                        torch.zeros(
-                            prev_hidden_lens[index], self.memory_dim, dtype=torch.float32, device=hidden_states.device
+                        -torch.zeros(
+                            prev_hidden_lens[index],
+                            self.memory_dim,
+                            dtype=hidden_states.dtype,
+                            device=hidden_states.device,
                         )
                         for index in range(len(self.current_conversations))
                     ],
                     batch_first=True,
-                    padding_value=torch.finfo(torch.float32).min,
+                    padding_value=torch.finfo(hidden_states.dtype).min,
                 )
                 .repeat_interleave(self.layer.memory.update_attention.num_heads, dim=0)
                 .transpose(1, 2)
             )
+            # print(module)
+            # print((memory_mask != 0).reshape(memory_mask.shape[0], -1).all(dim=1).any())
+            # print((attention_mask_mha != 0).reshape(attention_mask_mha.shape[0], -1).all(dim=1).any())
 
             altered_hidden_states, altered_memory_state = module.memory(
                 hidden_states,
@@ -199,7 +208,7 @@ class ContextManager:
         self.layer.register_forward_hook(hook, with_kwargs=True)
 
 
-class ContextHolder(TrainerCallback):
+class ContextHolder(TrainerCallback, nn.Module):
     def __init__(self, enc_layers_to_attach_memory, dec_layers_to_attach_memory, config):
         super().__init__()
         self.context_blocks = []

src/models/context/memory_cell.py
@@ -46,14 +46,17 @@ class MemoryCell(nn.Module):
         self.config = config
 
         # Initialize (0th time stamp) memory cells as parameters + positional embeddings (MxH)
-        self.hidden_init = nn.Parameter(torch.zeros(0, config.hidden_size))
-        self.memory_init = nn.Parameter(torch.zeros(config.memory_dim, config.hidden_size))
+        self.hidden_init = nn.Parameter(torch.randn(1, config.hidden_size))
+        self.memory_init = nn.Parameter(torch.randn(config.memory_dim, config.hidden_size))
 
         self.memory_positional_embeddings = LearnablePositionalEmbedding(config.memory_dim, config.hidden_size)
 
         # Initialize update memory
         self.update_attention = nn.MultiheadAttention(
-            config.hidden_size, num_heads=config.num_attention_heads, dropout=config.attention_dropout, batch_first=True
+            config.hidden_size,
+            num_heads=config.num_attention_heads,
+            dropout=config.attention_dropout,
+            batch_first=True,
         )
         self.update_norm1 = nn.LayerNorm(config.hidden_size)
         self.update_ff = FeedForward(config)
@@ -61,13 +64,16 @@ class MemoryCell(nn.Module):
 
         # Initialize output memory
         self.output_attention = nn.MultiheadAttention(
-            config.hidden_size, num_heads=config.num_attention_heads, dropout=config.attention_dropout, batch_first=True
+            config.hidden_size,
+            num_heads=config.num_attention_heads,
+            dropout=config.attention_dropout,
+            batch_first=True,
         )
         self.output_norm = nn.LayerNorm(config.hidden_size)
 
     def forward(self, hidden_states, prev_hidden_states, prev_memory_state, memory_mask=None, attention_mask_mha=None):
         """Actualize memory state"""
-
+        # print(f"hidden_states: {hidden_states}, prev_hidden_states: {prev_hidden_states}, prev_memory_state: {prev_memory_state}, memory_mask: {memory_mask}, attention_mask_mha: {attention_mask_mha}")
         """Update prev memory state with positional embeddings"""
         # Equation 4: Mt-1 = Mt-1 + PE(Mt-1)
         memory_state = self.memory_positional_embeddings(prev_memory_state)
@@ -76,7 +82,11 @@ class MemoryCell(nn.Module):
         # Equation 2: Mt-1_tilde = AddNorm(MHA(Mt-1, ht-1, ht-1))
         residual = memory_state
         temporal_memory_state, _ = self.update_attention(
-            memory_state, prev_hidden_states, prev_hidden_states, attn_mask=memory_mask
+            query=memory_state,
+            key=prev_hidden_states,
+            value=prev_hidden_states,
+            attn_mask=memory_mask,
+            need_weights=False,
         )
         temporal_memory_state = self.update_norm1(temporal_memory_state + residual)
 
@@ -89,7 +99,11 @@ class MemoryCell(nn.Module):
         residual = hidden_states
         # Equation 5: ht_tilde = MHA(ht, Mt, Mt)
         hidden_states, _ = self.output_attention(
-            hidden_states, current_memory_state, current_memory_state, attn_mask=attention_mask_mha
+            query=hidden_states,
+            key=current_memory_state,
+            value=current_memory_state,
+            attn_mask=attention_mask_mha,
+            need_weights=False,
         )
         hidden_states = hidden_states + residual
         hidden_states = self.output_norm(hidden_states)