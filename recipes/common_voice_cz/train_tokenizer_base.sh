8b4a7f27649964ec264fd319f15a2540dfd9d628
Lakoc <ipoloka@fit.vutbr.cz>
2024-04-23 09:54:44 +0200

    commonvoice setup lumi

--- recipes/common_voice_cz/train_tokenizer_base.sh
+++ recipes/common_voice_cz/train_tokenizer_base.sh
@@ -1,38 +1,52 @@
 #!/usr/bin/bash
-#SBATCH --job-name VOX
-#SBATCH --account OPEN-28-57
-#SBATCH --partition qcpu_exp
-#SBATCH --time 01:00:00
-#SBATCH --output=/mnt/proj1/open-28-58/lakoc/huggingface_asr/outputs/ebranchformer_voxpopuli_small_tokenizer.out
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=7
+#SBATCH --output="outputs/voxpopuli_czech/output_%x_%j.txt"
+#SBATCH --partition=small-g
+#SBATCH --mem=60G
+#SBATCH --time=1:00:00
+
+cd $SRC_DIR || exit
+
+srun --unbuffered --kill-on-bad-exit  singularity exec $SIFPYTORCH \
+/runscripts/conda-python-simple src/trainers/train_tokenizer.py \
+  --output_dir=$EXPERIMENT_PATH \
+  --preprocessing_num_workers="8" \
+  --datasets_creation_config="${RECIPE_DIR}/voxpopuli_cz.json" \
+  --writer_batch_size="200" \
+  --tokenizer_name="Lakoc/voxpopuli_uni50_cz" \
+  --vocab_size=50 \
+  --tokenizer_type="unigram" \
+  --text_column_name="text" \
+  --train_split="train" \
+  --pad_token="([pad])" \
+  --unk_token="([unk])" \
+  --bos_token="([bos])" \
+  --eos_token="([eos])" \
+  --mask_token="([mask])"
 
 EXPERIMENT="ebranchformer_cv_small_tokenizer"
-PROJECT="common_voice_cz"
-WORK_DIR="/mnt/proj1/open-28-58/lakoc/huggingface_asr"
-RECIPE_DIR="${WORK_DIR}/recipes/common_voice_cz"
-
-export HF_HOME="/scratch/project/open-28-57/lakoc/huggingface_cache"
-export PYTHONPATH="${PYTHONPATH}:${WORK_DIR}/src"
-export OMP_NUM_THREADS=64
-export WANDB_PROJECT=$PROJECT
-export WANDB_RUN_ID="${EXPERIMENT}"
-
-conda deactivate
-source activate loco_asr
-
+SRC_DIR="/project/${EC_PROJECT}/ipoloka/huggingface_asr"
+WORK_DIR="/scratch/${EC_PROJECT}/ipoloka/huggingface_asr"
+RECIPE_DIR="${SRC_DIR}/recipes/voxpopuli/czech"
 EXPERIMENT_PATH="${WORK_DIR}/experiments/${EXPERIMENT}"
 
-cd $WORK_DIR || exit
+module load LUMI partition/G PyTorch/2.2.0-rocm-5.6.1-python-3.10-singularity-20240209
 
-python src/trainers/train_tokenizer.py \
-  --max_duration_in_seconds="20.0" \
-  --min_duration_in_seconds="0.2" \
+export HF_HOME="/flash/${EC_PROJECT}/ipoloka/huggingface_cache"
+export PYTHONPATH="${PYTHONPATH}:${SRC_DIR}/src"
+
+cd $SRC_DIR || exit
+
+srun --unbuffered --kill-on-bad-exit  singularity exec $SIFPYTORCH \
+/runscripts/conda-python-simple src/trainers/train_tokenizer.py \
   --output_dir=$EXPERIMENT_PATH \
-  --length_column_name="input_len" \
-  --preprocessing_num_workers="128" \
-  --datasets_creation_config="${RECIPE_DIR}/common_voice_cz.json" \
+  --preprocessing_num_workers="8" \
+  --datasets_creation_config="${RECIPE_DIR}/voxpopuli_cz.json" \
   --writer_batch_size="200" \
-  --tokenizer_name="Lakoc/cv_uni50_cs" \
-  --vocab_size=500 \
+  --tokenizer_name="Lakoc/voxpopuli_uni50_cz" \
+  --vocab_size=50 \
   --tokenizer_type="unigram" \
   --text_column_name="text" \
   --train_split="train" \
