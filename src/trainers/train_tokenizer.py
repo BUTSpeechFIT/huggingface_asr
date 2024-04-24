c47c7673ee5292838263b1137593c987db3d1acf
Lakoc <ipoloka@fit.vutbr.cz>
2024-04-24 14:23:52 +0200

    train tokenizer fix for CTC

--- src/trainers/train_tokenizer.py
+++ src/trainers/train_tokenizer.py
@@ -77,6 +77,7 @@ def train_tokenizer(
         pad_token=pad_token,
         unk_token=unk_token,
         mask_token=mask_token,
+        sep_token="▁" if tokenizer_type == "unigram" else " ",
     )
 
     wrapped_tokenizer.push_to_hub(tokenizer_name)  # pylint: disable=not-callable
