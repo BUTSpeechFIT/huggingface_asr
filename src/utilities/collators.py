masking and logging fixes

src/utilities/collators.py
@@ -15,6 +15,8 @@ from transformers.models.wav2vec2.modeling_wav2vec2 import (
     _sample_negative_indices,
 )
 
+from models.encoders.e_branchformer import BestRQEBranchformerForPreTraining
+
 
 @dataclass
 class SpeechCollatorWithPadding:
@@ -148,7 +150,7 @@ class DataCollatorForWav2Vec2Pretraining:
     sampling_rate: Optional[int] = 16_000
     audio_path: str = None
     model_input_name: str = ""
-    min_masks: int = 1
+    min_masks: int = 2
 
     def __post_init__(self):
         if not isinstance(self.feature_extractor, (Wav2Vec2FeatureExtractor, Speech2TextFeatureExtractor)):
@@ -203,15 +205,17 @@ class DataCollatorForWav2Vec2Pretraining:
             min_masks=self.min_masks,
         )
 
-        # sample negative indices
-        sampled_negative_indices = _sample_negative_indices(
-            features_shape,
-            # pylint: disable=no-member
-            self.model.config.num_negatives,
-            mask_time_indices=mask_time_indices,
-        )
         batch["mask_time_indices"] = torch.tensor(mask_time_indices, dtype=torch.long, device=device)
-        batch["sampled_negative_indices"] = torch.tensor(sampled_negative_indices, dtype=torch.long, device=device)
+
+        if not isinstance(self.model, BestRQEBranchformerForPreTraining):
+            # sample negative indices
+            sampled_negative_indices = _sample_negative_indices(
+                features_shape,
+                # pylint: disable=no-member
+                self.model.config.num_negatives,
+                mask_time_indices=mask_time_indices,
+            )
+            batch["sampled_negative_indices"] = torch.tensor(sampled_negative_indices, dtype=torch.long, device=device)
 
         if self.model_input_name != self.feature_extractor.model_input_names[0]:
             batch[self.model_input_name] = batch[self.feature_extractor.model_input_names[0]]

src/utilities/training_utils.py
@@ -307,14 +307,17 @@ class SSLTrainer(Trainer):
 
     def gather_additional_statistics(self, inputs, outputs):
         additional_logs = {}
-        num_losses = inputs["mask_time_indices"].sum()
-        sub_attention_mask = inputs.pop("sub_attention_mask", None)
+        num_losses = inputs["mask_time_indices"].sum(dim=1)
+        sub_attention_mask = inputs.pop("attention_mask", None)
         sub_attention_mask = (
             sub_attention_mask
             if sub_attention_mask is not None
             else torch.ones_like(inputs["mask_time_indices"], device=inputs["mask_time_indices"].device)
         )
-        percent_masked = num_losses / sub_attention_mask.sum()
+
+        input_lens = self.model._get_feat_extract_output_lengths(sub_attention_mask.sum(dim=1))
+
+        percent_masked = (num_losses / input_lens).mean()
 
         if outputs.contrastive_loss:
             additional_logs["contrastive_loss"] = outputs.contrastive_loss
@@ -323,8 +326,12 @@ class SSLTrainer(Trainer):
             additional_logs["gumbel_temperature"] = torch.tensor(
                 self.gumbel_callback.current_gumbel_temperature, device=inputs["mask_time_indices"].device
             )
+        if outputs.codevector_perplexity is not None:
+            additional_logs["%_codebook_used"] = outputs.codevector_perplexity
+        if outputs.diversity_loss is not None:
+            additional_logs["%_unique_labels"] = outputs.diversity_loss
         additional_logs["%_mask_idx"] = percent_masked
-        additional_logs["num_losses"] = num_losses
+        additional_logs["num_losses"] = num_losses.sum()
 
         for key in additional_logs.keys():
             additional_logs[key] = additional_logs[key].detach()