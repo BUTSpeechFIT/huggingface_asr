be3acdce15ee5c554aa311139226cee8433eee5f
Lakoc <ipoloka@fit.vutbr.cz>
2024-05-21 20:03:30 +0200

    pure python job modif

--- cluster_utilities/LUMI/start_multinode_job_inside_env_pure_python.sh
+++ cluster_utilities/LUMI/start_multinode_job_inside_env_pure_python.sh
@@ -1,28 +1,44 @@
 #!/bin/bash -e
-# Source bash profile
-# shellcheck disable=SC1090
-# Report affinity
-echo "Rank $SLURM_PROCID --> $(taskset -p $$)"
-# Start conda environment inside the container
-$WITH_CONDA
+# Make sure GPUs are up
+if [ $SLURM_LOCALID -eq 0 ] ; then
+    rocm-smi
+fi
+sleep 2
 
-set -euo pipefail
+# MIOPEN needs some initialisation for the cache as the default location
+# does not work on LUMI as Lustre does not provide the necessary features.
+export MIOPEN_USER_DB_PATH="/tmp/$(whoami)-miopen-cache-$SLURM_NODEID"
+export MIOPEN_CUSTOM_CACHE_DIR=$MIOPEN_USER_DB_PATH
 
-export NODENAME=$(cat /proc/sys/kernel/hostname)
+# Set MIOpen cache to a temporary folder.
+if [ $SLURM_LOCALID -eq 0 ] ; then
+    rm -rf $MIOPEN_USER_DB_PATH
+    mkdir -p $MIOPEN_USER_DB_PATH
+fi
+sleep 2
+
+# Set interfaces to be used by RCCL.
+# This is needed as otherwise RCCL tries to use a network interface it has
+# noa ccess to on LUMI.
+export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
+export NCCL_NET_GDR_LEVEL=3
+
+# Set ROCR_VISIBLE_DEVICES so that each task uses the proper GPU
+export ROCR_VISIBLE_DEVICES=$SLURM_LOCALID
+
+# Report affinity to check
+echo "Rank $SLURM_PROCID --> $(taskset -p $$); GPU $ROCR_VISIBLE_DEVICES"
+
+# The usual PyTorch initialisations (also needed on NVIDIA)
+# Note that since we fix the port ID it is not possible to run, e.g., two
+# instances via this script using half a node each.
 export MASTER_ADDR=$(/runscripts/get-master "$SLURM_NODELIST")
-
-export MASTER_PORT=$(comm -23 <(seq 49152 65535) <(ss -tan | awk '{print $4}' | cut -d':' -f2 | grep "[0-9]\{1,5\}" | sort | uniq) | shuf | head -n 1)
-
-export WORLD_SIZE=$SLURM_NTASKS
+export MASTER_PORT=29500
+export WORLD_SIZE=$SLURM_NPROCS
 export RANK=$SLURM_PROCID
-export FS_LOCAL_RANK=$SLURM_PROCID
-export LOCAL_WORLD_SIZE=$SLURM_NTASKS_PER_NODE
 export LOCAL_RANK=$SLURM_LOCALID
 export NODE_RANK=$((($RANK - $LOCAL_RANK) / $LOCAL_WORLD_SIZE))
-
-# Redirect stdout and stderr so that we get a prefix with the node name
-exec > >(trap "" INT TERM; sed -u "s/^/$NODENAME:$LOCAL_RANK out: /")
-exec 2> >(trap "" INT TERM; sed -u "s/^/$NODENAME:$LOCAL_RANK err: /" >&2)
+export LOCAL_WORLD_SIZE=$SLURM_NTASKS_PER_NODE
 
 if [ $SLURM_LOCALID -eq 0 ] ; then
   if command -v rocm-smi &> /dev/null ; then
