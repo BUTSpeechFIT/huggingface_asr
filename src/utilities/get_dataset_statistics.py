fca94ca5c3196ab63e76fbb7cf433562a359ad1e
Lakoc <ipoloka@fit.vutbr.cz>
2024-06-27 09:49:52 +0200

    dataset statistics

---
+++ src/utilities/get_dataset_statistics.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+import datasets
+import numpy as np
+from transformers import HfArgumentParser
+
+
+@dataclass
+class DataTrainingArguments:
+    _argument_group_name = "Data related arguments"
+    """Dataset source related arguments."""
+    dataset_path: Optional[str] = field(
+        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
+    )
+    length_column_name: Optional[str] = field(
+        default=None, metadata={"help": "The name of the column containing the length of the audio files."}
+    )
+
+
+if __name__ == "__main__":
+    parser = HfArgumentParser((DataTrainingArguments))
+    (data_args,) = parser.parse_args_into_dataclasses()
+
+    df = datasets.load_from_disk(data_args.dataset_path)
+    print(df)
+    lengths = np.array(df[data_args.length_column_name])
+    # print statistics as mean, std, min, max
+    print(f"Mean: {lengths.mean()}")
+    print(f"Std: {lengths.std()}")
+    print(f"Min: {lengths.min()}")
+    print(f"Max: {lengths.max()}")
