6e70fed00b19b125517f95093bef7d12dc35cc4b
Lakoc <ipoloka@fit.vutbr.cz>
2024-10-12 13:28:26 +0200

    bestrq frozen 100h

---
+++ recipes/librispeech_ssl/30M_ebranchformer/lumi/finetune_frozen_libri100.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=4
+#SBATCH --tasks-per-node=4
+#SBATCH --cpus-per-task=7
+#SBATCH --output="outputs/librispeech_ssl/output_%x_%j.out"
+#SBATCH --error="outputs/librispeech_ssl/output_%x_%j.err"
+#SBATCH --partition=small-g
+#SBATCH --mem=200G
+#SBATCH --time=2-00:00:00
+
+
+EXPERIMENT="bestrq_30M_ebranchformer_pretrained_frozen_libri100"
+PROJECT="librispeech_ssl_v1_ft"
+
+SRC_DIR="/project/${EC_PROJECT}/ipoloka/huggingface_asr"
+WORK_DIR="/scratch/${EC_PROJECT}/ipoloka/huggingface_asr"
+RECIPE_DIR="${SRC_DIR}/recipes/librispeech_ssl"
+EXPERIMENT_PATH="${WORK_DIR}/experiments/${EXPERIMENT}"
+
+export HF_HOME="/scratch/project/open-28-57/lakoc/huggingface_cache"
+export PYTHONPATH="${PYTHONPATH}:${WORK_DIR}/src"
+export OMP_NUM_THREADS=64
+export WANDB_PROJECT="${PROJECT}"
+export WANDB_RUN_ID="${EXPERIMENT}"
+export WANDB_ENTITY="butspeechfit"
+
+
+module load LUMI PyTorch/2.2.0-rocm-5.6.1-python-3.10-singularity-20240209
+
+export OMP_NUM_THREADS=64
+
+export HF_HOME="/flash/${EC_PROJECT}/ipoloka/huggingface_cache"
+export PYTHONPATH="${PYTHONPATH}:${SRC_DIR}/src"
+export WANDB_PROJECT=$PROJECT
+export WANDB_RUN_ID="${EXPERIMENT}"
+
+
+cd $SRC_DIR || exit
+
+
+args=(
+  # General training arguments
+  --output_dir=$EXPERIMENT_PATH
+  --per_device_train_batch_size="64"
+  --per_device_eval_batch_size="64"
+  --dataloader_num_workers="4"
+  --num_train_epochs="500"
+  --group_by_length="True"
+  --do_train
+  --do_evaluate
+  --load_best_model_at_end
+
+  # Optimizer related arguments
+  --optim="adamw_torch"
+  --learning_rate="2e-3"
+  --warmup_steps="500"
+  --early_stopping_patience="5"
+  --weight_decay="1e-6"
+  --max_grad_norm="1.0"
+  --gradient_accumulation_steps="1"
+
+  # Logging, saving and evaluation related arguments
+  --report_to="wandb"
+  --logging_steps="10"
+  --save_strategy="epoch"
+  --evaluation_strategy="epoch"
+  --greater_is_better="False"
+  --save_total_limit="5"
+
+  # Data related arguments
+  --max_duration_in_seconds="20.0"
+  --min_duration_in_seconds="2.0"
+  --length_column_name="input_len"
+  --remove_unused_columns="False"
+  --preprocessing_num_workers="16"
+  --pad_to_multiples_of="100"
+  --datasets_creation_config="${RECIPE_DIR}/librispeech_100h.json"
+  --writer_batch_size="50"
+  --test_splits librispeech_test.clean librispeech_test.other librispeech_validation.clean librispeech_validation.other
+
+
+  # Preprocessing related arguments
+  --data_preprocessing_config="${SRC_DIR}/configs/default_data_preprocessing2d_pure.json"
+
+  # Model related arguments
+  --from_pretrained="/scratch/project_465000836/ipoloka/huggingface_asr/experiments/bestrq_30M_ebranchformer_pretrained_with_augment/checkpoint-52608/"
+  --feature_extractor_name="Lakoc/fe_mel_80_global_stats_librispeech"
+  --tokenizer_name="Lakoc/libri_1000"
+  --freeze_encoder
+  --config_overrides="finetune_with_additional_layer=True,finetune_with_layer_mixing=True"
+  )
+
+
+srun --unbuffered --kill-on-bad-exit  singularity exec $SIFPYTORCH \
+"${SRC_DIR}/cluster_utilities/LUMI/start_multinode_job_inside_env_pure_python.sh" src/trainers/train_ctc_asr.py "${args[@]}"
